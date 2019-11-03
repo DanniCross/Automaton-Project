c9b0cd020ca6a72d17d9d7c7b44d412080d242f2
Jose Cruz <josedanielcrzgmail.com>
2019-11-02 19:19:45 -0500

    Erase read json

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from Resources.CrossRiver import CrossRiver
 from Views.GUI import GUI
-from Resources.ReadJSON import Read
 
 
 def main():
     cross = CrossRiver()
-    read = Read(cross)
-    GUI(read)
+    GUI(cross)
 
 
 main()
